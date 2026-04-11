c57cd387ee9b159a0c9edb9c7ea387a99ec9851d
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-04-11 17:56:39 +0530

    Implement reverse integer function in Solution class

---
+++ 7lc.cpp
@@ -0,0 +1,22 @@
+#include <climits>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int revNum = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            // Overflow check
+            if (revNum > INT_MAX / 10 || revNum < INT_MIN / 10) {
+                return 0;
+            }
+
+            revNum = revNum * 10 + digit;
+            x = x / 10;
+        }
+
+        return revNum;
+    }
+};
