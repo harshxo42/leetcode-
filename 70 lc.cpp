f4a9cc9fd7f32e1a5b67204e553880b94ef8ebef
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-04-15 19:53:48 +0530

    Implement climbStairs function with memoization

---
+++ 70 lc.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return climb(n, memo);
+    }
+    
+private:
+    int climb(int n, vector<int>& memo) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (memo[n] != -1) return memo[n];
+        memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
+        return memo[n];
+    }
+};
