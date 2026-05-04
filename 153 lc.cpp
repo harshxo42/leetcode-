8fe140d03772c6cbdbaa5b3a1a1c6f931b513f3d
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-05-04 23:02:50 +0530

    Implement binary search to find minimum in rotated array

---
+++ 153 lc.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // If mid > right, minimum is in the right half (including mid+1)
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } 
+            // If mid < right, minimum is in the left half (including mid)
+            else {
+                right = mid;
+            }
+        }
+        
+        return nums[left];
+    }
+};
