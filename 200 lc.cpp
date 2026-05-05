11fe8c64a3af24f29143919cf5f8a067c564f500
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-05-05 22:27:30 +0530

    Implement numIslands function to count islands

---
+++ 200 lc.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+    
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+            return;
+        }
+        
+        // Mark as visited
+        grid[i][j] = '0';
+        
+        // Visit all neighbors (up, down, left, right)
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+};
