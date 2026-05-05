b1955c4978c1756d23916cce85aed2230ad1ef07
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-05-05 22:16:37 +0530

    Implement BSTIterator for in-order traversal

---
+++ 173 lc.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+private:
+    stack<TreeNode*> st;
+
+    void pushAllLeft(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushAllLeft(root);
+    }
+    
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        pushAllLeft(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
