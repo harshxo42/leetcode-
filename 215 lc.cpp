354f741365ddee3f84b1c2add9061e84c330360a
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-05-08 20:56:18 +0530

    Implement findKthLargest using a min-heap

---
+++ 215 lc.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // Min-heap to store k largest elements
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        for (int num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        
+        return minHeap.top();
+    }
+};
