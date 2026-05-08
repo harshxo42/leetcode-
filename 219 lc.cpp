9701423d7287be9b7d29884c502021b6c26ac3ad
Harsh <130823896+harshxo42@users.noreply.github.com>
2026-05-08 21:02:24 +0530

    Implement containsNearbyDuplicate function

---
+++ 219 lc.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> window;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (window.find(nums[i]) != window.end()) {
+                return true;
+            }
+            
+            window.insert(nums[i]);
+            
+            // Keep window size ≤ k
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+};
